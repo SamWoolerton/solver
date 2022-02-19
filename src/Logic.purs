3e16cfadd777911f321e42a7fa5535a5bd46d34b
Sam Woolerton <samuelwoolerton@gmail.com>
2022-02-20 11:32:57 +1300

    Move the guess duplicates check out of the filter loop to improve performance

--- src/Logic.purs
+++ src/Logic.purs
@@ -1,4 +1,13 @@
-module Logic where
+module Logic
+  ( CheckedGuess
+  , Score
+  , Word
+  , WordList
+  , filter_words
+  , is_correct_guess
+  , score_guess
+  , validate_guess
+  ) where
 
 import Prelude
 import Data.Array (any, elem, filter, length, mapWithIndex, nub)
@@ -23,9 +32,6 @@ type Score
     , partially_correct :: Int
     }
 
-valid_words :: WordList
-valid_words = Words.valid_words
-
 validate_guess :: Word -> Boolean
 validate_guess word = correct_length && acceptable_word
   where
@@ -39,32 +45,38 @@ is_correct_guess checked_guess = checked_guess.score.fully_correct == 5
 filter_words :: CheckedGuess -> WordList -> WordList
 filter_words { guess, score } previous_words = filter filter_fn previous_words
   where
+  has_dups = guess_has_duplicates guess
+
   filter_fn w = case score of
     { fully_correct: 0, partially_correct: 0 } -> not $ any char_in_guess (toCharArray w)
-    _ -> (score_guess guess w).score == score
+    _ -> (score_guess_impl guess w has_dups).score == score
 
   char_in_guess c = elem c $ toCharArray guess
 
 score_guess :: Word -> Word -> CheckedGuess
-score_guess guess answer = { guess, score: { fully_correct, partially_correct } }
+score_guess guess answer = score_guess_impl guess answer $ guess_has_duplicates guess
+
+guess_has_duplicates :: Word -> Boolean
+guess_has_duplicates guess = (length guess_letters) == (length $ nub guess_letters)
+  where
+  guess_letters = (toCharArray guess)
+
+score_guess_impl :: Word -> Word -> Boolean -> CheckedGuess
+score_guess_impl guess answer guess_has_dups = { guess, score: { fully_correct, partially_correct } }
   where
   count_true arr = length $ filter identity arr
 
   fully_correct = count_true $ check_word_exact guess answer
 
-  partially_correct = count_true $ check_word_partial guess answer
+  partially_correct = count_true $ check_word_partial guess answer guess_has_dups
 
 check_word_exact :: Word -> Word -> Array Boolean
 check_word_exact guess answer = mapWithIndex (\i c -> check_letter_exact i c answer) (toCharArray guess)
 
-check_word_partial :: Word -> Word -> Array Boolean
-check_word_partial guess answer = fn guess answer
+check_word_partial :: Word -> Word -> Boolean -> Array Boolean
+check_word_partial guess answer guess_has_dups = fn guess answer
   where
-  guess_letters = (toCharArray guess)
-
-  has_duplicates = (length guess_letters) == (length $ nub guess_letters)
-
-  fn = if has_duplicates then check_word_partial_dups else check_word_partial_no_dups
+  fn = if guess_has_dups then check_word_partial_dups else check_word_partial_no_dups
 
 check_letter_exact :: Int -> String -> Word -> Boolean
 check_letter_exact i c answer = Just c == answer_char
